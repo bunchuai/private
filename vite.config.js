9b4b849d8f16ebdbb65bcdd7947ffd1549656c6a
NB-66-08-03\NB-66-08-03 <it.pakdpu@gmail.com>
2026-09-11 14:40:52 +0700

    Use root base for dev, /private/ only on GH Pages build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/private/",
+  base: process.env.GH_PAGES ? "/private/" : "/",
   plugins: [react()],
 });
